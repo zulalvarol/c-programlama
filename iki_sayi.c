fa035d3cdf77483e436b3a052a9518c9ec3803d8
Zülal Varol <73954361+zulalvarol@users.noreply.github.com>
2020-11-04 19:31:54 +0300

    Add files via upload
    Girilen 2 sayiyi ekrana yazdirir.

---
+++ iki_sayi.c
@@ -0,0 +1,12 @@
+//Girilen 2 sayiyi ekrana yazdirir.
+#include<stdio.h>
+main(){
+       
+       int sayi1,sayi2;
+       
+       printf("2 sayi giriniz: ");
+       scanf("%d %d",&sayi1,&sayi2);
+       
+       printf("Girdiginiz 2 sayi %d ve %d'dir.",sayi1,sayi2);       
+       getch();
+       }
